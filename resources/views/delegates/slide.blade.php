b82dff8f23830c4867d49a9d569fa99c2e39c88a
minhan39 <phamminhan.0107@gmail.com>
2024-02-17 20:20:44 +0700

    Update image styling and prevent scrolling

--- resources/views/delegates/slide.blade.php
+++ resources/views/delegates/slide.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-    </style>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    body {
+      overflow: hidden;
+    }
+  </style>
 </head>
 <body>
     <img id="image" />
@@ -18,12 +21,13 @@
 
       socket.on("info", (data) => {
         // Assuming `data` is an array of objects with the structure you provided
-
         console.log(data);
         let storageUrl = <?php echo $storage ?>;
         document.getElementById("image").src = storageUrl + "/" + data.delegate.image;     
-        document.getElementById("image").style.height = "100%";   
-        document.getElementById("image").style.width = "auto";   
+        document.getElementById("image").style.objectFit = "cover";
+        document.getElementById("image").style.width = "100vw";
+        document.getElementById("image").style.height = "100vh";
+        document.body.style.overflow = "hidden"; // Prevent scrolling in both x and y directions
       });
     </script>
 </body>
